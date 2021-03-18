1080e32e7a70886890c957072a890e1d9583ea8e
mikeusa04 <71799572+mikeusa04@users.noreply.github.com>
2021-03-18 01:53:46 -0700

    week1 assignment

---
+++ week1/math/app.js
@@ -0,0 +1,7 @@
+const mathObj = require('./math')
+
+console.log(mathObj);
+console.log(mathObj.sum(5, 2));
+console.log(mathObj.subtract(11, 2));
+console.log(mathObj.multiply(10, 3));
+console.log(mathObj.divide(9, 2));
